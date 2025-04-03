52ef79fcbb3b145c46de958df32bf3dad40794c2
s_ueno <pandapark09@yahoo.co.jp>
2025-04-03 10:56:27 +0900

    移動処理作成

---
+++ Assets/MoveTiger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTiger : MonoBehaviour
+{
+    float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Application.targetFrameRate = 60;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //this. 
+    }
+}
